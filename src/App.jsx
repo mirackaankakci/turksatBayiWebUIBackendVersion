bda2c46da2fcea3a5db6f0aabe623dfae21ac05e
mirackaankakci <kaankakcii34@gmail.com>
2025-05-22 20:44:36 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './index.css'
 import logo from './assets/logo.png'
 
@@ -15,7 +15,6 @@ function App() {
   return (
     <div>
     <ResponsiveMenu />
-    <CampaignBanner />
 
     </div>
   )
